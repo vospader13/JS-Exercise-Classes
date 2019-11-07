dd26ddb182e4983e04790e6d8592297bcfc74528
unknown <justenmcdonald@gmail.com>
2019-11-06 20:07:59 -0700

    completed first challenge

--- index.js
+++ index.js
@@ -41,8 +41,28 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood){
+    if (someFood <= 10 ){
+      this.stomach.push(someFood);
+    }
+  }
+  poop(){
+    if (this.stomach.length >= 10) {
+      this.stomach = []
+    }
+  }
+  toString(){
+    return `${this.name} ${this.age}`;
+  }
 }
+
+
+
 
 /*
   TASK 2
